0bf177bbdf5ca614024c45806c5ed0ece79352b6
SolessChong <solesschong@gmail.com>
2013-10-09 21:59:51 +0800

    added distance between nodes-nodes, node-trajectory, traj-traj

--- wxg/trajana.py
+++ wxg/trajana.py
@@ -17,6 +17,7 @@ Xiaogang Wang [06]
 `Learning Semantic Scene Models by Trajectory Analysis`, ECCV
 """
 
+
 class Node(object):
 	"""
 	Node is the component of trajectory.
@@ -24,12 +25,44 @@ class Node(object):
 	"""
 
 	def __init__(self, pos, v):
-		self.pos = pos
+		self.pos = pos[0:2]
 		self.v = v
+
+	def spatial_dist_to(self, x):
+		"""
+		Spatial distance to another node
+		Defined by Euclidean distance
+
+		This is the function `h` in Wang[06]
+		"""
+		d = self.pos - x.pos
+		return np.sqrt(d.dot(d))
+
+	def feature_dist_to(self, x):
+		"""
+		Feature distance to another node
+
+		This is the function `d` in Wang[06]
+		"""
+
+		return 0
+
+	def dist_to(self, x):
+		return self.spatial_dist_to(x) + 1 * self.feature_dist_to(x)
 
 	def to_string(self):
 		rst = 'x=%d, y=%d' % (self.x, self.y)
 		return rst
+
+	def nearest_node_in(self, trajectory):
+		"""
+		Finds the index of the node in a trajectory that is
+		nearest to this node
+
+		This is the `phi` function in Wang[06]
+		"""
+		dists = [self.dist_to(node) for node in trajectory.nodes]
+		return np.argmin(dists)
 
 # end class Node
 
@@ -48,17 +81,17 @@ class Trajectory(object):
 		self.firstFrame = tracklet.firstFrame
 		self.nFrames = tracklet.nFrames
 
-	def __sub__(self, x):
-		"""
-		Returns the distance between `self` and `x`
-		"""
-		return self.dist_to(x)
-
-	def dist_to(self, x):
+	def dist_to(self, trajectory):
 		"""
 		Returns the distance between 'self' and 'x'
 		"""
-		return 3
+		dists = []
+		for i in range(len(self.nodes)):
+			nodeA = self.nodes[i]
+			nodeB = nodeA.nearest_node_in(trajectory)
+			dists.append(nodeA.dist_to(nodeB)
+
+		return np.average(dists)
 
 # end class Trajectory
 
